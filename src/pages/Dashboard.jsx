899bc7b44ebe50bc7f4ac177487da77b42cb7360
H82M19\Student <Student>
2025-04-06 14:50:46 +0700

    fourth commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TopNavbar from '../components/TopNavBar'
+import Overview from '../components/Overview'
 
 function Dashboard() {
   return (
     <div>
       <TopNavbar />
+      <Overview />
     </div>
   )
 }
